d440ad82e3e231495ffda7bd08ef074438f9f7fb
light_2000 <pkazuki2000@docomo.ne.jp>
2021-09-23 16:43:38 +0900

    練習

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          react環境構築
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {increment,decrement} from '../actions';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+import reactDom from 'react-dom';
+
+class App extends Component {
+  render(){
+    const props = this.props
+
+    return(
+      <React.Fragment>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state =>({value: state.count.value})
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
